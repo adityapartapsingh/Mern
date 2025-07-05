ac9aa27b00492ac19ce8f1461e925c0138b2d770
Aditya Partap Singh <adityapartapsingh92@gmail.com>
2025-07-05 22:41:29 +0530

    q12

---
+++ questions/q12.js
@@ -0,0 +1,14 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+  }
+}
+
+const person1 = new Person("Alice", 25);
+
+person1.sayHello(); 
